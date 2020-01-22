2ac36f2200f1e4c9c5ff66f4cb35f0b27fd8f46b
briggirl2 <briggirl2@gmail.com>
2020-01-22 18:31:07 +0900

    02. login

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.urls import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # app
+    'accounts',
     'blog',
 ]
 
@@ -123,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = reverse_lazy('login')
+
+# LOGIN_REDIRECT_URL = reverse_lazy('profile')
+
