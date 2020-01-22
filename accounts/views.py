31dcc487f9b529aa2395c3f1947e3258bec5ac28
briggirl2 <briggirl2@gmail.com>
2020-01-22 23:34:24 +0900

    08. next arg during signup

--- accounts/views.py
+++ accounts/views.py
@@ -25,7 +25,9 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect(settings.LOGIN_URL)
+            # return redirect(settings.LOGIN_URL)
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {
@@ -46,7 +48,9 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        # return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
