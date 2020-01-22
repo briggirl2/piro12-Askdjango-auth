b8e644da61523a6d977d3906acc6741868d46f15
briggirl2 <briggirl2@gmail.com>
2020-01-22 19:04:12 +0900

    profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    request.user
+    # logout: django.contrib.auth.models.AnonymousUser
+    # login: django.contrib.auth.models.User
+
+    return render(request, 'accounts/profile.html')
